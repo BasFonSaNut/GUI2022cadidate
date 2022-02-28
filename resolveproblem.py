294cbef80521677586b089c0060ff8343cdd431c
BasFonSaNut <nenomelt@gmail.com>
2022-02-28 13:53:39 +0700

    update edit button

--- resolveproblem.py
+++ resolveproblem.py
@@ -15,7 +15,20 @@ from datetime import datetime,timedelta
 # print(len(a))
 # print(a[13:-4]) //not work in python
 
-s = "3900080009999"
-res = s.partition('9999')[0]
-print(res)
+# s = "3900080009999"
+# res = s.partition('9999')[0]
+# print(res)
 
+
+
+date_1 = '27-Feb-2022 00:37'
+date_2 = '28-Feb-2022 00:21'
+date_format_str = '%d-%b-%Y %H:%M'
+start = datetime.strptime(date_1, date_format_str)
+end =   datetime.strptime(date_2, date_format_str)
+# Get interval between two timstamps as timedelta object
+diff = end - start
+# Get interval between two timstamps in hours
+diff_in_hours = diff.total_seconds() / 3600
+print('Difference between two datetimes in hours:')
+print(diff_in_hours)
